971dcf61789c4205acb4aedcb4de2b63567cd2e0
Vila <alexandrevn.cc@gmail.com>
2017-04-17 19:34:19 -0300

    Update Lista1.py

--- Lista1.py
+++ Lista1.py
@@ -1,4 +1,23 @@
-<h1>LISTA 1 </h1>
-# Funcional
-l = list(range(20))
-l
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "metadata": {
+    "collapsed": true
+   },
+   "source": [
+    "# Programação Funcional Lista 1\n",
+    "## Aluno : Luiz Vieira Gonzaga Neto \n",
+    "## Matricula : 383842\n",
+    "## Curso Ciência da Computação\n",
+    "### Feito com &hearts; em  Python 3"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "\n",
+    "## 1. Descreva a saída dos seguintes comandos. OBS: “l” é uma lista, preencha-a a maneira que achar melhor."
+   ]
+  },
